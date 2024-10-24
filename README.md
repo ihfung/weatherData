5c6ee744825ead0a8d4972797b01c9cbceff28fc
ihleung <ihfungleung@gmail.com>
2024-10-24 10:10:59 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weatherData
+
+This is a simple weather data analysis using Python. 
+Simply by asking the user to enter a city name, the program will return the current weather data of the city.
+We get the weather data by using the wttr.in API.
+
---
+++ weatherData2.py
@@ -0,0 +1,29 @@
+#method two to get weather data
+
+# Importing the requests module
+import requests
+
+# # Sending request to get the IP location information
+# res = requests.get('https://ipinfo.io/')
+# data = res.json()  # Receiving the response in JSON format
+
+# # Extracting the location of the city from the response
+# citydata = data['city']
+# print("Current Location:", citydata)
+
+# # Passing the city name to the URL to get weather data
+# url = 'https://wttr.in/{}'.format(citydata)
+# res = requests.get(url)
+
+# # Printing the schematic weather details of the city
+# print(res.text)
+
+# Getting user input for the city name
+city = input("Enter the city name: ")
+
+# Passing the city name to the URL to get weather data
+url = f'https://wttr.in/{city}'
+res = requests.get(url)
+
+# Printing the schematic weather details of the city
+print(res.text)
